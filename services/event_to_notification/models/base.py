1a729f450faa716e0062d50f57e0d7bfdec51f65
Dmitrii <kachkindmitry@gmail.com>
2022-05-30 23:48:38 +0300

    fast json

--- services/event_to_notification/models/base.py
+++ services/event_to_notification/models/base.py
@@ -1,12 +1,14 @@
 import orjson
 from pydantic import BaseModel
 
+
 def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
 
 class FastJsonModel(BaseModel):
     """Модель с быстрым json-сериализатором."""
+
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
