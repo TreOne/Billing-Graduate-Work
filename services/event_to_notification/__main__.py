4706ad658bf1c42291dceef07def77b181bca1eb
Dmitrii <kachkindmitry@gmail.com>
2022-06-02 14:48:21 +0300

    main module

--- services/event_to_notification/__main__.py
+++ services/event_to_notification/__main__.py
@@ -1,18 +1,22 @@
-from handlers import send_notification_to_admin, send_notification_to_user
-from message_handler import MessageHandler
+import logging
 
+from core.settings import settings
+from services.base_services import Consumer
+from services.message_handler import MessageHandler
 
 message_handler = None
+logger = logging.getLogger(__name__)
 
 
 def main():
+    consumer = Consumer()
+    messages = consumer.consume()
     for message in messages:
-        message_handler.handle(message.title, message.body)
+        message_handler.handle(title=message.key.decode('UTF-8'), message=message.value)
 
 
 if __name__ == '__main__':
     message_handler = MessageHandler()
-    message_handler.register('bill.created', send_notification_to_user)
-    message_handler.register('bill.refunded', send_notification_to_user)
-    message_handler.register('bill.refunded', send_notification_to_admin)
+    for task in settings.tasks:
+        message_handler.register(task.title, task.method)
     main()
