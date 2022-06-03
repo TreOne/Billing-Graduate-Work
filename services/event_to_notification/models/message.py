bfb02e170466d17b260f0e377535f4c37522091d
Kachkin Dmitrii <kachkindmitry@gmail.com>
2022-06-03 23:15:21 +0300

    jinja template data model

--- services/event_to_notification/models/message.py
+++ services/event_to_notification/models/message.py
@@ -1,7 +1,16 @@
+from pydantic import BaseModel
+
 from models.base import FastJsonModel
 
 
-class IncomingNotificationSchema(FastJsonModel):
+class TemplateBodySchema(BaseModel):
+    """ Represents  template data model ."""
+    link: str = "https://pastseason.ru/"
+    user: str
+    amount: int
+
+
+class NotificationSchema(FastJsonModel):
     """ Represents message to send model ."""
     recipient: str
     subject: str
