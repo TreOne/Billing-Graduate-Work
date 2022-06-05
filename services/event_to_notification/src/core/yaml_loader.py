34bf1b5008beecd249179a0bcdb077748e6a0c2e
Kachkin Dmitrii <kachkindmitry@gmail.com>
2022-06-05 23:13:11 +0300

    custom func loader removed

--- services/event_to_notification/src/core/yaml_loader.py
+++ services/event_to_notification/src/core/yaml_loader.py
@@ -4,23 +4,9 @@ import yaml
 from pydantic import BaseSettings
 
 
-def func_loader(loader, node):
-    """A loader for functions."""
-    params = loader.construct_mapping(node)
-    module = __import__(params["module"], fromlist=[params["name"]])
-    return getattr(module, params["name"])
-
-
-def get_loader():
-    """Return a yaml loader."""
-    loader = yaml.Loader
-    loader.add_constructor("!Func", func_loader)
-    return loader
-
-
 def yaml_settings_source(settings: BaseSettings) -> dict[str, any]:
-    """Возвращает настройки из файла settings.yaml."""
+    """Loads settings from settings.yaml."""
     settings_path = Path(__file__).parent / 'settings.yaml'
     with settings_path.open('r', encoding='utf-8') as f:
-        yaml_settings = yaml.load(f, Loader=get_loader())
+        yaml_settings = yaml.load(f, Loader=yaml.Loader)
     return yaml_settings
