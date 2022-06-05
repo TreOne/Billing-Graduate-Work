20128380d0794ed0060840b03425bd58bde0a996
Kachkin Dmitrii <kachkindmitry@gmail.com>
2022-06-05 23:12:31 +0300

    abstract property

--- services/event_to_notification/src/services/kafka_consumer.py
+++ services/event_to_notification/src/services/kafka_consumer.py
@@ -1,22 +1,27 @@
-__all__ = ["ConsumerKafka"]
+__all__ = ['ConsumerKafka']
 
 import logging
+from abc import abstractmethod, ABC
 from typing import Optional, NamedTuple
 
 import backoff
 from kafka import KafkaConsumer
 
 from core.settings import get_settings, KafkaTaskSettings
-from services.base_services import AbstractConsumer
+from services.abstract_services import AbstractConsumer
 
 logger = logging.getLogger(__name__)
 settings = get_settings()
 
 
-class ConsumerKafka(AbstractConsumer):
-    def __init__(self, configs: KafkaTaskSettings):
+class ConsumerKafka(AbstractConsumer, ABC):
+    @property
+    @abstractmethod
+    def configs(self) -> KafkaTaskSettings:
+        pass
+
+    def __init__(self):
         self.consumer: Optional[KafkaConsumer] = None
-        self.configs = configs
 
     @backoff.on_exception(
         wait_gen=backoff.expo,
@@ -26,11 +31,10 @@ class ConsumerKafka(AbstractConsumer):
     def start_consumer(self) -> None:
         if not self.consumer:
             self.consumer: KafkaConsumer = KafkaConsumer(
-                security_protocol="PLAINTEXT",
+                security_protocol='PLAINTEXT',
                 bootstrap_servers=self.configs.bootstrap_servers,
                 auto_offset_reset=self.configs.auto_offset_reset,
                 enable_auto_commit=self.configs.enable_auto_commit,
-                key_deserializer=None,
                 group_id=self.configs.group_id,
                 reconnect_backoff_ms=100,
             )
