c63fd27eb2a68b80b40cf0a04d0fa31341d7dc6e
Kachkin Dmitrii <kachkindmitry@gmail.com>
2022-06-06 23:34:32 +0300

    base Abstract notification service

---
+++ services/event_to_notification/src/services/notification_api/base.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod, ABC
+
+from pydantic import BaseModel
+
+
+class Notification(BaseModel):
+    """Исходящее сообщение."""
+    recipient: str
+    subject: str
+    body: str
+    immediately: bool = False
+    log_it: bool = False
+    ttl: int = 60 * 60 * 24
+
+
+class AbstractNotificationService(ABC):
+    """Интерфейс отправки уведомлений."""
+    @abstractmethod
+    def send(self, notification: Notification) -> bool:
+        """Отправляет уведомление, возвращает успешность отправки."""
+        raise NotImplementedError
