8bfb8ecd8d7201fc40f3e5ad61bfa9685e9ecdae
Kachkin Dmitrii <kachkindmitry@gmail.com>
2022-06-06 23:31:19 +0300

    notification service

---
+++ services/event_to_notification/src/services/notification_api/notification_service.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+import requests
+
+from services.notification_api.base import AbstractNotificationService, Notification
+
+
+class NotificationAPI(AbstractNotificationService):
+    """Реализация интерфейса отправки уведомлений."""
+    def __init__(self, api_url: str):
+        self.api_url = api_url
+
+    def send(self, notification: Notification) -> bool:
+        response = requests.post(self.api_url, data=notification.dict())
+        return response.status_code == HTTPStatus.OK
