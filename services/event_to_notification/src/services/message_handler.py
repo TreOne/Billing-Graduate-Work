018233cbe53951197556bcf382ef7f498b5656d6
Kachkin Dmitrii <kachkindmitry@gmail.com>
2022-06-07 23:59:50 +0300

    Tre : notification handler refactoring

--- services/event_to_notification/src/services/message_handler.py
+++ services/event_to_notification/src/services/message_handler.py
@@ -1,14 +1,14 @@
-__all__ = ['MessageHandler']
-
 from typing import Callable
 
-from core.settings import Settings
+from services.auth_api.base import AbstractAuth
+from services.notification_api.base import AbstractNotificationService
 
 
 class MessageHandler:
-    def __init__(self, settings: Settings):
+    def __init__(self, user_auth_service: AbstractAuth, notification_service: AbstractNotificationService):
         self.__observers: dict[str, list[Callable]] = dict()
-        self.settings: Settings = settings
+        self.user_auth_service = user_auth_service
+        self.notification_service = notification_service
 
     def register(self, title: str, handler: Callable) -> None:
         handlers = self.__observers.setdefault(title, [])
@@ -17,4 +17,8 @@ class MessageHandler:
     def handle(self, title: str, message: str) -> None:
         handlers = self.__observers.get(title, [])
         for handler in handlers:
-            handler(message=message, settings=self.settings)
+            handler(
+                message=message,
+                user_auth_service=self.user_auth_service,
+                notification_service=self.notification_service
+            )
