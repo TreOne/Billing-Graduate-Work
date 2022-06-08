b85d997c8f93374529da2caec40f609a1e25e669
Kubashov, Timur <timur.kubashov@x5.ru>
2022-06-08 23:09:35 +0300

    Добавил загрузку в бд админов

--- services/auth_api/src/auth_api/manage.py
+++ services/auth_api/src/auth_api/manage.py
@@ -56,6 +56,52 @@ def loaddata():
     with open(contract_roles_filename) as f:
         roles = json.load(f)
     for role in roles:
+        existing_role = session.query(Role).get(role['uuid'])
+        if existing_role:
+            click.echo(f'{existing_role.name} already created!')
+            continue
         session.add(Role(name=role['code'], uuid=role['uuid']))
     session.commit()
     click.echo(f'Load roles: {len(roles)}')
+
+    contract_admins_filename = os.path.join(dirname, 'settings/contract_admins.json')
+    role_admin = session.query(Role).get("c45ea0ef-f9b4-4569-af09-9ee7b0a9c16c")
+    with open(contract_admins_filename) as f:
+        admins = json.load(f)
+    for admin in admins:
+        username = admin['login']
+        password = admin['password']
+
+        existing_admin = session.query(User).filter(User.username == username).first()
+        if existing_admin:
+            click.echo(f'{existing_admin.username} already created!')
+            continue
+        new_admin = User(username=username, password=password)
+        session.add(new_admin)
+        new_admin.roles.append(role_admin)
+    session.commit()
+    click.echo(f'Load admins: {len(admins)}')
+
+
+@click.command()
+@with_appcontext
+def loadadmins():
+    """Добавление администраторов."""
+    dirname = os.path.dirname(__file__)
+    contract_admins_filename = os.path.join(dirname, 'settings/contract_admins.json')
+    role_admin = session.query(Role).get("c45ea0ef-f9b4-4569-af09-9ee7b0a9c16c")
+    with open(contract_admins_filename) as f:
+        admins = json.load(f)
+    for admin in admins:
+        username = admin['login']
+        password = admin['password']
+
+        existing_admin = session.query(User).filter(User.username == username).first()
+        if existing_admin:
+            click.echo(f'{existing_admin.username} already created!')
+            return
+        new_admin = User(username=username, password=password)
+        session.add(new_admin)
+        new_admin.roles.append(role_admin)
+    session.commit()
+    click.echo(f'Load admins: {len(admins)}')
